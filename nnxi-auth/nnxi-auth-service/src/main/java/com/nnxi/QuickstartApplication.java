ab7acde6c1113ec56dca6d27a9f4104ae213a6f7
benhyland-team <yanglangh@gmail.com>
2019-08-08 22:28:21 +0800

    添加项目配置

--- nnxi-auth/nnxi-auth-service/src/main/java/com/nnxi/QuickstartApplication.java
+++ nnxi-auth/nnxi-auth-service/src/main/java/com/nnxi/QuickstartApplication.java
@@ -1,5 +1,6 @@
 package com.nnxi;
 
+import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.nnxi"})
 @MapperScan("com.nnxi.mapper")
-//@DubboComponentScan(basePackages = "com.nnxi.service.impl")
+@DubboComponentScan(basePackages = "com.nnxi.service.impl")
 public class QuickstartApplication {
 
     public static void main(String[] args) {
--- nnxi-auth/nnxi-auth-service/src/main/java/com/nnxi/service/impl/UserImpl.java
+++ nnxi-auth/nnxi-auth-service/src/main/java/com/nnxi/service/impl/UserImpl.java
@@ -4,8 +4,9 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.nnxi.mapper.UserMapper;
 import com.nnxi.model.User;
 import com.nnxi.service.IUserService;
-//import org.apache.dubbo.config.annotation.Service;
-//
-//@Service(interfaceClass = IUserService.class)
+import org.apache.dubbo.config.annotation.Service;
+
+
+@Service(interfaceClass = IUserService.class)
 public class UserImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 }
